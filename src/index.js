22623f6a14a4820da748ccbcf058a09475cf3785
Your Name <your-email@example.com>
2024-08-02 11:40:01 +0530

    city table search

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { PORT } = require("./config/serverConfig");
 const port =PORT||3000;
 const setUpAndStartServer=async ()=>{
     const app=express();
-    app.use(bodyParser.json());w
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}))
     app.listen(port,()=>{
         console.log(`server started at port ${port}`)
