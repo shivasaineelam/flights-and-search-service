3ac5df9ff8394ebee51140a7af8293a35ed9e452
Your Name <your-email@example.com>
2024-08-02 15:41:59 +0530

    city-repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser")
 const { PORT } = require("./config/serverConfig");
+const CityRepository = require("./repository/city-repository");
 
 const port =PORT||3000;
 const setUpAndStartServer=async ()=>{
---
+++ src/repository/city-repository.js
@@ -0,0 +1,48 @@
+const { where } = require("sequelize");
+const { City } = require("./../models/index");
+
+class CityRepository {
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name });
+      return city;
+    } catch (error) {
+        console.log("something went wrong in the repository layer")
+      throw error;
+    }
+  }
+  async deleteCity({ cityId }) {
+    try {
+      await City.destroy({ where: { id: { cityId } } });
+      return true;
+    } catch (error) {
+        throw error;
+    }
+  }
+  async updateCity(cityId,data){
+    try {
+        const city=await City.update(data,{
+            where:{
+                id:cityId
+            }
+        });
+        return city;
+        
+    } catch (error) {
+        throw error;
+    }
+  }
+  async  getCity(cityId) {
+    try {
+        const city=await City.findByPk(cityId);
+        return city;
+        
+    } catch (error) {
+        throw error;
+        
+    }
+    
+  }
+}
+
+module.exports =CityRepository
